61abce199e734e4e9745a0f3b2bfbbd77934b375
arfinhosainn <developerarfin@gmail.com>
2025-11-30 20:49:49 +0800

    Implement HttpClientFactory

---
+++ core/domain/src/commonMain/kotlin/com/scelio/brainest/domain/logging/BrainestLogger.kt
@@ -0,0 +1,9 @@
+package com.scelio.brainest.domain.logging
+
+
+interface BrainestLogger {
+    fun debug(message: String)
+    fun info(message: String)
+    fun warn(message: String)
+    fun error(message: String, throwable: Throwable? = null)
+}

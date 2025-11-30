d98fee3d0a61df15c9708dd0b4a7e08145f31434
arfinhosainn <developerarfin@gmail.com>
2025-11-30 19:05:15 +0800

    Implement network call utility

--- core/data/src/commonMain/kotlin/com/scelio/brainest/data/networking/HttpClientExt.kt
+++ core/data/src/commonMain/kotlin/com/scelio/brainest/data/networking/HttpClientExt.kt
@@ -2,8 +2,17 @@ package com.scelio.brainest.data.networking
 
 import com.scelio.brainest.domain.util.DataError
 import com.scelio.brainest.domain.util.Result
+import io.ktor.client.HttpClient
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 
 
@@ -11,6 +20,74 @@ expect suspend fun <T> platformSafeCall(
     execute: suspend () -> HttpResponse,
     handleResponse: suspend (HttpResponse) -> Result<T, DataError.Remote>
 ): Result<T, DataError.Remote>
+
+suspend inline fun <reified Request, reified Response: Any> HttpClient.post(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        post {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Response: Any> HttpClient.get(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        get {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Response: Any> HttpClient.delete(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        delete {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Request, reified Response: Any> HttpClient.put(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        put {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
 
 suspend inline fun <reified T> safeCall(
     noinline execute: suspend () -> HttpResponse
@@ -26,7 +103,7 @@ suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<
     return when(response.status.value) {
         in 200..299 -> {
             try {
-                com.scelio.brainest.domain.util.Result.Success(response.body<T>())
+                Result.Success(response.body<T>())
             } catch(e: NoTransformationFoundException) {
                 Result.Failure(DataError.Remote.SERIALIZATION)
             }

e6ffaf3dea7096ccfe9d6b860b3b3727b4f5721c
arfinhosainn <developerarfin@gmail.com>
2025-12-01 15:20:44 +0800

    Build register screen skeleton

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/register/RegisterScreen.kt
@@ -0,0 +1,40 @@
+package com.scelio.brainest.presentation.register
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.scelio.brainest.designsystem.BrainestTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterRoot(
+    viewModel: RegisterViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    RegisterScreen(
+        state = state,
+        onAction = viewModel::onAction
+    )
+}
+
+@Composable
+fun RegisterScreen(
+    state: RegisterState,
+    onAction: (RegisterAction) -> Unit,
+) {
+
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    BrainestTheme {
+        RegisterScreen(
+            state = RegisterState(),
+            onAction = {}
+        )
+    }
+}

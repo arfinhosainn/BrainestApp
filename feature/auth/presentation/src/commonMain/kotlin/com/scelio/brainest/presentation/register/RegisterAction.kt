b61c80bc2e14fe7d45b7721a12eb4c64709af244
arfinhosainn <developerarfin@gmail.com>
2025-12-01 21:42:22 +0800

    Implement register screen UI

--- feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/register/RegisterAction.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/register/RegisterAction.kt
@@ -3,4 +3,8 @@ package com.scelio.brainest.presentation.register
 
 sealed interface RegisterAction {
 
+    data object OnLoginClick: RegisterAction
+    data object OnInputTextFocusGain: RegisterAction
+    data object OnRegisterClick: RegisterAction
+    data object OnTogglePasswordVisibilityClick: RegisterAction
 }
--- feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/register/RegisterScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/register/RegisterScreen.kt
@@ -1,10 +1,36 @@
 package com.scelio.brainest.presentation.register
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import brainest.feature.auth.presentation.generated.resources.Res
+import brainest.feature.auth.presentation.generated.resources.email
+import brainest.feature.auth.presentation.generated.resources.email_placeholder
+import brainest.feature.auth.presentation.generated.resources.login
+import brainest.feature.auth.presentation.generated.resources.password
+import brainest.feature.auth.presentation.generated.resources.password_hint
+import brainest.feature.auth.presentation.generated.resources.register
+import brainest.feature.auth.presentation.generated.resources.username
+import brainest.feature.auth.presentation.generated.resources.username_hint
+import brainest.feature.auth.presentation.generated.resources.username_placeholder
+import brainest.feature.auth.presentation.generated.resources.welcome_to_brainest
 import com.scelio.brainest.designsystem.BrainestTheme
+import com.scelio.brainest.designsystem.components.brand.BrainestBrandLogo
+import com.scelio.brainest.designsystem.components.buttons.BrainestButton
+import com.scelio.brainest.designsystem.components.buttons.BrainestButtonStyle
+import com.scelio.brainest.designsystem.components.layouts.BrainestAdaptiveFormLayout
+import com.scelio.brainest.designsystem.components.layouts.BrainestSnackbarScaffold
+import com.scelio.brainest.designsystem.components.textfields.BrainestPasswordTextField
+import com.scelio.brainest.designsystem.components.textfields.BrainestTextField
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -13,9 +39,12 @@ fun RegisterRoot(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
+    val snackbarHostState = remember { SnackbarHostState() }
+
     RegisterScreen(
         state = state,
-        onAction = viewModel::onAction
+        onAction = viewModel::onAction,
+        snackbarHostState = snackbarHostState
     )
 }
 
@@ -23,8 +52,78 @@ fun RegisterRoot(
 fun RegisterScreen(
     state: RegisterState,
     onAction: (RegisterAction) -> Unit,
+    snackbarHostState: SnackbarHostState
 ) {
+    BrainestSnackbarScaffold(
+        snackbarHostState = snackbarHostState
+    ) {
+        BrainestAdaptiveFormLayout(
+            headerText = stringResource(Res.string.welcome_to_brainest),
+            errorText = state.registrationError?.asString(),
+            logo = { BrainestBrandLogo() }
+        ) {
+            BrainestTextField(
+                state = state.usernameTextState,
+                placeholder = stringResource(Res.string.username_placeholder),
+                title = stringResource(Res.string.username),
+                supportingText = state.usernameError?.asString()
+                    ?: stringResource(Res.string.username_hint),
+                isError = state.usernameError != null,
+                onFocusChanged = { isFocused ->
+                    onAction(RegisterAction.OnInputTextFocusGain)
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            BrainestTextField(
+                state = state.emailTextState,
+                placeholder = stringResource(Res.string.email_placeholder),
+                title = stringResource(Res.string.email),
+                supportingText = state.emailError?.asString(),
+                isError = state.emailError != null,
+                onFocusChanged = { isFocused ->
+                    onAction(RegisterAction.OnInputTextFocusGain)
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            BrainestPasswordTextField(
+                state = state.passwordTextState,
+                placeholder = stringResource(Res.string.password),
+                title = stringResource(Res.string.password),
+                supportingText = state.passwordError?.asString()
+                    ?: stringResource(Res.string.password_hint),
+                isError = state.passwordError != null,
+                onFocusChanged = { isFocused ->
+                    onAction(RegisterAction.OnInputTextFocusGain)
+                },
+                onToggleVisibilityClick = {
+                    onAction(RegisterAction.OnTogglePasswordVisibilityClick)
+                },
+                isPasswordVisible = state.isPasswordVisible
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
+            BrainestButton(
+                text = stringResource(Res.string.register),
+                onClick = {
+                    onAction(RegisterAction.OnRegisterClick)
+                },
+                enabled = state.canRegister,
+                isLoading = state.isRegistering,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            BrainestButton(
+                text = stringResource(Res.string.login),
+                onClick = {
+                    onAction(RegisterAction.OnLoginClick)
+                },
+                style = BrainestButtonStyle.SECONDARY,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
 }
 
 @Preview
@@ -33,7 +132,8 @@ private fun Preview() {
     BrainestTheme {
         RegisterScreen(
             state = RegisterState(),
-            onAction = {}
+            onAction = {},
+            snackbarHostState = remember { SnackbarHostState() }
         )
     }
 }

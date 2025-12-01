b139fb24bcacb884091c95e57234b6d215ea7b42
arfinhosainn <developerarfin@gmail.com>
2025-12-01 13:12:00 +0800

    Implemented BrainestAdaptiveFormLayout

--- core/designsystem/src/commonMain/kotlin/com/scelio/brainest/designsystem/components/layouts/BrainestSurface.kt
+++ core/designsystem/src/commonMain/kotlin/com/scelio/brainest/designsystem/components/layouts/BrainestSurface.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -34,6 +36,8 @@ fun BrainestSurface(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
             header()
             Surface(

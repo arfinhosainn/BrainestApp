fedf6b71aeb1d606ce219d1f60ac48acf17db6dc
arfinhosainn <developerarfin@gmail.com>
2025-12-01 13:45:29 +0800

    Implement BrainestSimpleSuccessLayout

---
+++ core/designsystem/src/commonMain/kotlin/com/scelio/brainest/designsystem/components/layouts/BrainestSimpleSuccessLayout.kt
@@ -0,0 +1,100 @@
+package com.scelio.brainest.designsystem.components.layouts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.scelio.brainest.designsystem.BrainestTheme
+import com.scelio.brainest.designsystem.components.brand.BrainestSuccessIcon
+import com.scelio.brainest.designsystem.components.buttons.BrainestButton
+import com.scelio.brainest.designsystem.components.buttons.BrainestButtonStyle
+import com.scelio.brainest.designsystem.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BrainestSimpleSuccessLayout(
+    title: String,
+    description: String,
+    icon: @Composable () -> Unit,
+    primaryButton: @Composable () -> Unit,
+    secondaryButton: @Composable (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        icon()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = -(25).dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.extended.textPrimary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.extended.textSecondary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            primaryButton()
+
+            if(secondaryButton != null) {
+                Spacer(modifier = Modifier.height(8.dp))
+                secondaryButton()
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+@Preview
+fun BrainestSimpleSuccessLayoutPreview() {
+    BrainestTheme (darkTheme = false) {
+        BrainestSimpleSuccessLayout(
+            title = "Hello world!",
+            description = "Test description",
+            icon = {
+                BrainestSuccessIcon()
+            },
+            primaryButton = {
+                BrainestButton(
+                    text = "Log In",
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            },
+            secondaryButton = {
+                BrainestButton(
+                    text = "Resend verification email",
+                    onClick = {},
+                    style = BrainestButtonStyle.SECONDARY,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+        )
+    }
+}

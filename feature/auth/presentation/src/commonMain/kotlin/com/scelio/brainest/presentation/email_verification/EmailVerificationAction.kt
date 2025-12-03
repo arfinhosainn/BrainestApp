65b951805691da1a2595cb984bd45cd9e3179313
arfinhosainn <developerarfin@gmail.com>
2025-12-03 23:25:02 +0800

    Implement email verification UI

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/email_verification/EmailVerificationAction.kt
@@ -0,0 +1,6 @@
+package com.scelio.brainest.presentation.email_verification
+
+sealed interface EmailVerificationAction {
+    data object OnLoginClick: EmailVerificationAction
+    data object OnCloseClick: EmailVerificationAction
+}

3d9997cc5a8e188e939d3fa16a30cc62072286c6
arfinhosainn <developerarfin@gmail.com>
2025-12-03 22:39:11 +0800

    Set up navigation between register & register success screens

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/navigation/AuthGraph.kt
@@ -0,0 +1,28 @@
+package com.scelio.brainest.presentation.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.scelio.brainest.presentation.register.RegisterRoot
+import com.scelio.brainest.presentation.register_success.RegisterSuccessRoot
+
+fun NavGraphBuilder.authGraph(
+    navController: NavController,
+    onLoginSuccess: () -> Unit,
+) {
+    navigation<AuthGraphRoutes.Graph>(
+        startDestination = AuthGraphRoutes.Register
+    ) {
+        composable<AuthGraphRoutes.Register> {
+            RegisterRoot(
+                onRegisterSuccess = {
+                    navController.navigate(AuthGraphRoutes.RegisterSuccess(it))
+                }
+            )
+        }
+        composable<AuthGraphRoutes.RegisterSuccess> {
+            RegisterSuccessRoot()
+        }
+    }
+}

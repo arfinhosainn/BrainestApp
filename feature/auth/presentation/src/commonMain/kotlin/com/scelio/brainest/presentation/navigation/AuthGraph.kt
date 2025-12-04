d0b4e45cfb52fc7a8055448f34b938d283414694
arfinhosainn <developerarfin@gmail.com>
2025-12-04 23:13:59 +0800

    Implement login screen UI

--- feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/navigation/AuthGraph.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/navigation/AuthGraph.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import com.scelio.brainest.presentation.login.LoginRoot
 import com.scelio.brainest.presentation.register.RegisterRoot
 import com.scelio.brainest.presentation.register_success.RegisterSuccessRoot
 
@@ -12,13 +13,42 @@ fun NavGraphBuilder.authGraph(
     onLoginSuccess: () -> Unit,
 ) {
     navigation<AuthGraphRoutes.Graph>(
-        startDestination = AuthGraphRoutes.Register
+        startDestination = AuthGraphRoutes.Login
     ) {
+
+        composable<AuthGraphRoutes.Login> {
+            LoginRoot(
+                onLoginSuccess = onLoginSuccess,
+                onForgotPasswordClick = {
+                    navController.navigate(AuthGraphRoutes.ForgotPassword)
+                },
+                onCreateAccountClick = {
+                    navController.navigate(AuthGraphRoutes.Register) {
+                        restoreState = true
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+
         composable<AuthGraphRoutes.Register> {
             RegisterRoot(
                 onRegisterSuccess = {
                     navController.navigate(AuthGraphRoutes.RegisterSuccess(it))
+                },
+                onLoginClick = {
+                    navController.navigate(AuthGraphRoutes.Login) {
+                        popUpTo(AuthGraphRoutes.Register) {
+                            inclusive = true
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 }
+
+
             )
         }
         composable<AuthGraphRoutes.RegisterSuccess> {

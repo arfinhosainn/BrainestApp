890bfb24742a433ef00af946121b97571743d232
arfinhosainn <developerarfin@gmail.com>
2025-12-05 19:20:10 +0800

    Implement forgot password functionality

--- feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/forgot_password/ForgotPasswordScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/forgot_password/ForgotPasswordScreen.kt
@@ -3,10 +3,13 @@ package com.scelio.brainest.presentation.forgot_password
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -14,18 +17,21 @@ import brainest.feature.auth.presentation.generated.resources.Res
 import brainest.feature.auth.presentation.generated.resources.email
 import brainest.feature.auth.presentation.generated.resources.email_placeholder
 import brainest.feature.auth.presentation.generated.resources.forgot_password
+import brainest.feature.auth.presentation.generated.resources.forgot_password_email_sent_successfully
 import brainest.feature.auth.presentation.generated.resources.submit
 import com.scelio.brainest.designsystem.BrainestTheme
 import com.scelio.brainest.designsystem.components.brand.BrainestBrandLogo
 import com.scelio.brainest.designsystem.components.buttons.BrainestButton
 import com.scelio.brainest.designsystem.components.layouts.BrainestAdaptiveFormLayout
 import com.scelio.brainest.designsystem.components.textfields.BrainestTextField
+import com.scelio.brainest.designsystem.extended
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun ForgotPasswordRoot(
-    viewModel: ForgotPasswordViewModel = viewModel()
+    viewModel: ForgotPasswordViewModel = koinViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
@@ -53,8 +59,8 @@ fun ForgotPasswordScreen(
                 .fillMaxWidth(),
             placeholder = stringResource(Res.string.email_placeholder),
             title = stringResource(Res.string.email),
-            isError = state.emailError != null,
-            supportingText = state.emailError?.asString(),
+            isError = state.errorText != null,
+            supportingText = state.errorText?.asString(),
             keyboardType = KeyboardType.Email,
             singleLine = true
         )
@@ -69,6 +75,17 @@ fun ForgotPasswordScreen(
             enabled = !state.isLoading && state.canSubmit,
             isLoading = state.isLoading
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        if(state.isEmailSentSuccessfully) {
+            Text(
+                text = stringResource(Res.string.forgot_password_email_sent_successfully),
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.extended.success,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 

87913305e46c3f4d004b4b02d1700fb39b89860f
arfinhosainn <developerarfin@gmail.com>
2025-12-08 11:30:05 +0800

    Implement chat list item ui

--- feature/chat/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/chat_list/components/ChatListItemUi.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/chat_list/components/ChatListItemUi.kt
@@ -1,39 +1,130 @@
 package com.scelio.brainest.presentation.chat_list.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import brainest.feature.chat.presentation.generated.resources.Res
+import brainest.feature.chat.presentation.generated.resources.arrow_right
+import brainest.feature.chat.presentation.generated.resources.improve_style
 import com.scelio.brainest.designsystem.BrainestTheme
-import com.scelio.brainest.designsystem.extended
-import com.scelio.brainest.domain.models.ChatMessage
-import kotlinx.atomicfu.TraceBase.None.append
-import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import kotlin.time.Clock
 
 @Composable
-fun ChatListItemUi(
+fun ChatItemUI(
+    title: String,
+    onClick: () -> Unit,
+    leadingIcon: Painter? = null,
+    iconSize: Dp = 24.dp,
+    iconTint: Color = Color.Unspecified,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium,
+    textColor: Color = MaterialTheme.colorScheme.onSurface,
+    fontFamily: FontFamily = FontFamily.Default,
+    fontWeight: FontWeight = FontWeight.Normal,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colorScheme.secondaryContainer,
+    cornerRadius: Dp = 20.dp,
+    elevation: Dp = 0.2.dp
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+        shape = RoundedCornerShape(cornerRadius),
+        elevation = CardDefaults.cardElevation(defaultElevation = elevation)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .clip(RoundedCornerShape(cornerRadius))
+                .background(backgroundColor)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = ripple(bounded = true)
+                ) { onClick() }
+                .padding(horizontal = 20.dp, vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.weight(1f)
+            ) {
+                leadingIcon?.let { icon ->
+                    Icon(
+                        painter = icon,
+                        modifier = Modifier.size(iconSize),
+                        contentDescription = null,
+                        tint = iconTint
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                }
 
-){
+                Text(
+                    text = title,
+                    style = textStyle,
+                    color = textColor,
+                    fontFamily = fontFamily,
+                    fontWeight = fontWeight,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
 
+            Icon(
+                painter = painterResource(Res.drawable.arrow_right),
+                contentDescription = "Navigate",
+                tint = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChatItemUIPreview() {
+    BrainestTheme(darkTheme = false) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(16.dp)
+        ) {
+            ChatItemUI(
+                leadingIcon = painterResource(Res.drawable.improve_style),
+                title = "Solving Algorithm Problem in Kotlin and Spring Boot",
+                onClick = { /* Handle click */ }
+            )
+        }
+    }
 }

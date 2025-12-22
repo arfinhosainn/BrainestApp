50a8f79adef4c7ec0b12d0edc60073f3d8cb1daa
arfinhosainn <developerarfin@gmail.com>
2025-12-22 23:01:11 +0800

    Designed chat attachment button

--- feature/chat/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/chat_detail/components/AttachmentFabMenu.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/scelio/brainest/presentation/chat_detail/components/AttachmentFabMenu.kt
@@ -1,15 +1,23 @@
 package com.scelio.brainest.presentation.chat_detail.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,10 +27,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import brainest.feature.chat.presentation.generated.resources.Res
+import brainest.feature.chat.presentation.generated.resources.ic_camera
+import brainest.feature.chat.presentation.generated.resources.ic_document
+import brainest.feature.chat.presentation.generated.resources.ic_gallery
 import brainest.feature.chat.presentation.generated.resources.ic_mic
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun AttachmentFabMenu(
@@ -33,65 +46,87 @@ fun AttachmentFabMenu(
 ) {
     var expanded by remember { mutableStateOf(false) }
 
-    Box(modifier = modifier.size(40.dp)) {
-        // Main FAB button
+    Box(modifier = modifier.size(40.dp).background(Color.Transparent)) {
         FloatingActionButton(
             onClick = { expanded = !expanded },
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape),
-            containerColor = Color(0xFFEAE6E2),
-            contentColor = Color.Black.copy(alpha = 0.75f),
+            containerColor = Color(0xFF787880).copy(alpha = 0.30f),
+            contentColor = MaterialTheme.colorScheme.onBackground,
             elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 0.dp),
             shape = CircleShape,
         ) {
             Icon(
-                imageVector = Icons.Default.Add,
+                imageVector = if (expanded) Icons.Default.Close else Icons.Default.Add,
                 contentDescription = "Open attachment menu",
                 modifier = Modifier.size(20.dp)
             )
         }
 
-        // Dropdown Menu
         DropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false }
+            onDismissRequest = { expanded = false },
+            containerColor = Color.Transparent,
+            shadowElevation = 0.dp,
+            offset = DpOffset((-10).dp, 0.dp)
+
         ) {
-            // Gallery option
             DropdownMenuItem(
-                text = { Text("Pick from Gallery") },
+                text = { Text("Take Photo") },
                 onClick = {
                     expanded = false
                     onGalleryClick()
                 },
                 leadingIcon = {
-                    Icon(
-                        painter = painterResource(Res.drawable.ic_mic),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp),
-                        tint = Color.Black.copy(alpha = 0.75f)
-                    )
+                    IconButton(
+                        onClick = {},
+                        colors = IconButtonColors(
+                            containerColor = Color(0xFF787880).copy(alpha = 0.30f),
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color(0xFFEAE6E2),
+                            disabledContentColor = Color.Black
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.ic_camera),
+                            contentDescription = null,
+                            modifier = Modifier.size(22.dp),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
                 }
             )
 
-            // Camera option
+            Spacer(Modifier.height(15.dp))
+
             DropdownMenuItem(
-                text = { Text("Capture with Camera") },
+                text = { Text("Upload Photo") },
                 onClick = {
                     expanded = false
                     onCameraClick()
                 },
                 leadingIcon = {
-                    Icon(
-                        painter = painterResource(Res.drawable.ic_mic),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp),
-                        tint = Color.Black.copy(alpha = 0.75f)
-                    )
+                    IconButton(
+                        onClick = {},
+                        colors = IconButtonColors(
+                            containerColor = Color(0xFF787880).copy(alpha = 0.30f),
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color(0xFFEAE6E2),
+                            disabledContentColor = Color.Black
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.ic_gallery),
+                            contentDescription = null,
+                            modifier = Modifier.size(22.dp),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
                 }
             )
+            Spacer(Modifier.height(15.dp))
 
-            // Document option
             DropdownMenuItem(
                 text = { Text("Upload File") },
                 onClick = {
@@ -99,14 +134,34 @@ fun AttachmentFabMenu(
                     onDocumentClick()
                 },
                 leadingIcon = {
-                    Icon(
-                        painter = painterResource(Res.drawable.ic_mic),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp),
-                        tint = Color.Black.copy(alpha = 0.75f)
-                    )
+                    IconButton(
+                        onClick = {},
+                        colors = IconButtonColors(
+                            containerColor = Color(0xFF787880).copy(alpha = 0.30f),
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color(0xFFEAE6E2),
+                            disabledContentColor = Color.Black
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.ic_document),
+                            contentDescription = null,
+                            modifier = Modifier.size(22.dp),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
                 }
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewAttachmentFabMenu() {
+    AttachmentFabMenu(
+        onGalleryClick = {},
+        onCameraClick = {},
+        onDocumentClick = {}
+    )
 }
